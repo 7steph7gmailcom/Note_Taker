2c5440bd80b651b108d51195a8177876211f49ef
stephaniecjohansson <7steph7@gmail.com>
2021-09-06 17:41:20 -0600

    saverouting

--- routes/API_Route_index.js
+++
@@ -1,3 +0,0 @@
-var path = require("path");
-var Router = require('./modules/router/router');
-var router = new Router(path.join(__dirname,'routes'));
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = function(app){
+
+    app.get("/notes", function(req, res){
+        res.sendFile(path.join(__dirname, "../../notes.html"));
+    });
+
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../../index.html"));
+    });
+
+
+}
+
+
+
+// module.exports = function(app){
+
+//     app.get("/notes", function(req, res){
+//         res.sendFile(path.join(__dirname, "../../notes.html"));
+//     });
+
+//     app.get("/", function(req, res){
+//         res.sendFile(path.join(__dirname, "../public/index.html"));
+//     });
+
+
+// }
