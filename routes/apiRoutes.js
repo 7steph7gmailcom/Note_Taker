6f5a572c3b4d81b8579d8fe96a4db35b23c28638
stephaniecjohansson <7steph7@gmail.com>
2021-09-09 23:51:46 -0600

    404error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,19 @@
-const fs = require("fs");
-const notesData = require("../routes/apiRoutes.js");
+// const fs = require("fs");
+// const noteData = require("./db/db.json");
 
 
-module.exports = function(app){
+// module.exports = function(app){
 
-    function writeTodataBase(notes){
+// const append = (content, file) => {
+//   fs.readFile(file, "utf8", (err, data) => {
+//     if (err) {
+//         throw err;
+//      } else {
+//         const note = JSON.parse(data);
+//         note.push(content);
+//         writeToFile(file, note);
+//      }
+//    });
+//  };
 
-    notes = JSON.stringify(notes);
-     console.log (notes);
-    fs.writeFileSync("./db/db.json", notes, function(err){
-    if (err) {
-        return console.log(error);
-        }
-    });
-}
-
-// GET Method to return "Notes"
-app.get("/api/notes", function(req, res){
-        res.json(notesData);
-    });
-
-
-// POST Method to add "Notes"
-app.post("/api/notes", function(req, res){
-
-        if (notesData.length == 0){
-            req.body.id = "0";
-        } else{
-            req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
-        }
-        console.log("req.body.id: " + req.body.id);
-    
-    notesData.push(req.body);
-
-    writeTodataBase(notesData);
-    console.log(notesData);
-
-    res.json(req.body);
-});
-}
+// }
--- server.js
+++ server.js
@@ -2,56 +2,69 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// const notesData = require("./routes/apiRoutes");
-// const notesTaken = require("./routes/htmlRoutes");
-// const {_dirname } = require("path");
-
+const uuid = require("./uuid");
+const util = require("util");
+const noteData = require("./db/db.json");
+const readFromFile = util.promisify(fs.readFile);
 
 // Create server application at port 3001 (localhost)
-// const routes = routes('./routes');
-
 const PORT = process.env.PORT || 3001;
 
 // Read JSON (middleware)
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-// app.use(express.static(path.join(_dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // app.use(routes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
+});
   
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html")),
-);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.get("/api/notes", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
-  });
+});
 
 const writeToFile = (destination, note) => {
-  fs.writeFile(destination, JSON.stringify(note, null, 4), (err) =>
-      err ? console.error(err) : console.info("Notes Updated")
-    );
-  };
-
+    fs.writeFile(destination, JSON.stringify(note, null, 4), (err) =>
+        err ? console.error(err) : console.info("Notes Updated")
+      );
+    };
 
 const append = (content, file) => {
-  fs.readFile(file, "utf8", (err, data) => {
-    if (err) {
-        throw err;
-      } else {
-        const note = JSON.parse(data);
-        note.push(content);
-        writeToFile(file, note);
-      }
-    });
-  };
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+          throw err;
+        } else {
+          const note = JSON.parse(data);
+          note.push(content);
+          writeToFile(file, note);
+        }
+      });
+    };
 
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if(title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+   };
+   append(newNote, "./db/db.json");
+   const response = {
+     status: "Note Posted!",
+     body: newNote,
+   };
+   res.status(201).json(response);
+ } else {
+   res.status(500).json("Error with note posting!");
+ }
+});
 
 // Include js files
 // require("./routes/apiRoutes")(app);
@@ -63,11 +76,7 @@ const append = (content, file) => {
 // app.use(express.static("public"));
 
 // Add listener (localhost 3001)
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
 })
-
-
-
-
 
