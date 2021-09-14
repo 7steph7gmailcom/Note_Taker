3a7f4ee178850ac2ac122e83b03ce765f0bcbc13
stephaniecjohansson <7steph7@gmail.com>
2021-09-14 16:46:58 -0600

    heroku

--- server.js
+++ server.js
@@ -17,17 +17,6 @@ app.use(express.json());
 app.use(express.static("public"));
 // app.use(routes);
 
-
-// Include js files
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
-
-// app.use("/api", notesData)
-// app.use("/", notesTaken)
-// // Use public folder
-
-
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
